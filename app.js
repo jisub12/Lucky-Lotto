89fa0578ea18d7a23967ff38d28b4eabd6e6c898
jisub12 <jisub11@nate.com>
2023-06-07 22:12:00 +0900

    배포전

---
+++ app.js
@@ -0,0 +1,22 @@
+// express cors
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors({
+    origin : "http://localhost:7777/",
+    credentials : true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'lotto.html'))
+})
+
+app.listen(7777, ()=> {
+    console.log("7777 Server Open")
+})
